63991f8d6fbb57b27e2ed8bcf619b886d2b226af
sali <sali.taymor@gmail.com>
2022-06-20 23:36:29 +0200

    nouns moved to homepage

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 import express from "express";
 import { siteData } from "./src/models.js";
-import { siteView } from "./src/views.js";
+
 import path from "path";
 
 const __dirname = path.resolve(path.dirname(""));
 const app = express();
-const port = process.env.PORT || 3007;
+const port = process.env.PORT || 3008;
 
 app.use(express.static("public"));
 // damit sagen wir dass die viws in public views pages sind
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "./public/views"));
+app.set("views", path.join(__dirname, "./src/views"));
 // hier zeigen wir die data wir renderen die data
 //send in model data to the view as we did before
 
 app.get("/", (req, res) => {
-    res.render("pages/index", siteData);
+res.render("pages/index", siteData);
 });
-console.log(siteData)
+app.get("/nouns", (req, res) => {
+res.render("pages/nouns", siteData);
+});
+app.get("/books", (req, res) => {
+res.render("pages/books", siteData);
+});
+// console.log(siteData)
 
 //das ist alte version 
 // app.get('/', (req, res) => {
